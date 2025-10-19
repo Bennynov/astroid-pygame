39b62849cf90ba923fa777f816564fed8debfdf0
Ben <bendolkemade@gmail.com>
2025-10-19 17:09:18 +0200

    C: new fps limit set and the start of the next part

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
-import constants
 from constants import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 > 0:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         screen.fill((0, 0, 0))
-        pygame.display.flip
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        pygame.display.flip
+        
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
